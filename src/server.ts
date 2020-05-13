e26d80b767c1754766f87fb8881d218d24e24f11
João Melo <jopcmelo@gmail.com>
2020-05-13 18:13:02 -0300

    chore: make the starter point

---
+++ src/server.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (request, response) => response.json({ message: 'hello world' }))
+
+app.listen(3333, () => console.log('🚀 Server started on port 3333!'));
