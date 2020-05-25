06540889c4d7c00fba5501ef3c71bef0731452f7
João Melo <jopcmelo@gmail.com>
2020-05-25 15:02:35 -0300

    chore: Add user tokens migrations and typeorm repository

---
+++ src/modules/users/infra/http/controllers/ResetPasswordController.ts
@@ -0,0 +1,15 @@
+import { container } from 'tsyringe';
+import { Request, Response } from 'express';
+
+import ResetPasswordService from '@modules/users/services/ResetPasswordService';
+
+export default class ResetPasswordController {
+  async create(request: Request, response: Response): Promise<Response> {
+    const { password, token } = request.body;
+
+    const resetPassword = container.resolve(ResetPasswordService);
+    await resetPassword.execute({ token, password });
+
+    return response.status(204).json();
+  }
+}
